627a492cdf14aaf6df6b0c42c698925d728c743c
GavinTafoya <83233027+GavinTafoya@users.noreply.github.com>
2024-03-01 22:17:34 -0700

    Bing

--- artifact.php
+++ artifact.php
@@ -1,20 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-$pagetitle = "Artifacts - Gavin Tafoya"
+    $pagetitle = "Artifacts - Gavin Tafoya";
     include ".head";
 ?>
 <body>
-    <header>
-        <h1>Gavin Tafoya</h1>
-    </header>
-    <nav>
-        <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="artifact.html">Artifacts</a></li>
-            <li><a href="contact.html">Contact</a></li>
-        </ul>
-    </nav>
+    <?php
+        include ".header";
+    ?>
     <main>
         <section class="title">
             <h2>About Me</h2>
