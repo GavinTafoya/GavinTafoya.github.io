f344e73dd53b70dd2dc47f5466a03a5e1bc78b11
GavinTafoya <83233027+GavinTafoya@users.noreply.github.com>
2024-03-03 03:42:45 +0000

    bing

--- contact.php
+++ contact.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-    $pagetitle = "Contact";
-    include "head";
-?>
-<body>
-    <?php
-        include "header";
-    ?>
-    <main>
-        <section class="title">
-            <h2>About Me</h2>
+  <?php
+      $pagetitle = "Contact";
+      include "head";
+  ?>
+  <body>
+    <div class="container">
+      <?php include "header"; ?>
+
+        <main>
+          <section class="title">
+              <h2>About Me</h2>
+          </section>
+          <section class="artifact">
             <p>and my journey</p>
-        </section>
-    </main>
-</body>
-<?php
-    include "footer";
-?>
+          </section>
+        </main>
+
+      <?php include "footer"; ?>
+    </div>
+  </body>
 </html>
--- index.php
+++ index.php
@@ -5,17 +5,18 @@
     include "head";
 ?>
 <body>
-    <?php
-        include "header";
-    ?>
-    <main>
-        <section class="title">
-            <h2>About Me</h2>
-            <p>and my journey</p>
-        </section>
-    </main>
+  <div class="container">
+    <?php include "header"; ?>
+      <main>
+          <section class="title">
+              <h2>About Me</h2>
+          </section>
+          <section class="about">
+              <p>I am Gavin Tafoya, a student at Boise State University. I am currently     studying Games, Interactive Media and Mobile Technologies and earning a minor in Computer Science. I am also a</p>
+          </section>
+      </main>
+    <?php include "footer"; ?>
+  </div>
 </body>
-<?php
-    include "footer";
-?>
+ 
 </html>
